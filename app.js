2a9cd9a0040d9485231b707c1f485ee45231eac2
Ahmad101abu <>
2021-06-16 12:27:10 +0000

    the app script

---
+++ app.js
@@ -0,0 +1,28 @@
+var name = prompt ("what is your name: ")
+alert("welcome to TRRIFIC PHONE "+ name )
+var fav = prompt("what do you like more:iPhone 12 ,Galaxy S21 5G,or Note 20 ")
+
+function myFunction() {
+  confirm("Press a button!");
+}
+if (fav == "iPhone 12"){
+document.write("<p>"+"wow it is ثxcellent choice "+"</p>")
+}
+else if (fav == "Galaxy S21 5G"){
+  document.write(
+ "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuUeiBVkWUFVbNZCEwSt1RjP66V4OhixDM7Q&usqp=CAU' alt='s21'>")
+
+ 
+     
+
+
+// "</div>")
+  
+
+}
+else if (fav == "Note 20"){
+  document.write("<p>"+"yea we have this nice phone enjoy!"+"</p>")
+
+}
+else 
+alert("sorry we dont have this phone "+name + "have great day ")
