a9566444446ce4d3ebd536d5427e3f784b599f7b
Ahmad101abu <>
2021-06-17 12:42:07 +0000

    new css

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var name = prompt ("what is your name: ")
 alert("welcome to TRRIFIC PHONE "+ name )
-var fav = prompt("what do you like more:iPhone 12 ,Galaxy S21 5G,or Note 20 ")
+var fav = prompt("what do you like more:iPhone 12,Galaxy S21 5G,orNote 20")
 
 function myFunction() {
   confirm("Press a button!");
 }
+
+while (fav != "iPhone 12"&& fav !="Galaxy S21 5G"&& fav !="Note 20"){
+     fav=prompt("pleas try again")
+    
+}
 if (fav == "iPhone 12"){
-document.write("<p>"+"wow it is ثxcellent choice "+"</p>")
+ var numpic=prompt("how many times you want to see this photo")
+ for(var x=0; x < numpic ;x++)
+ document.write(  
+    "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb_MdZIjOQpdweL1F3TdDeF82Ad_ab6geqJA&usqp=CAU'>" )
+document.write("<p>"+"wow it is excellent choice "+"</p>")
 }
 else if (fav == "Galaxy S21 5G"){
   document.write(
@@ -19,7 +28,7 @@ else if (fav == "Galaxy S21 5G"){
 // "</div>")
   
 
-}
+  }
 else if (fav == "Note 20"){
   document.write("<p>"+"yea we have this nice phone enjoy!"+"</p>")
 
